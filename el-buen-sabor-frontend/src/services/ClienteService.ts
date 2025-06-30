04224fef83c0c294d6019ecd96ebc0775e437b8b
KOmoROid <135452272+KOmoROid@users.noreply.github.com>
2025-06-30 00:32:16 -0300

    Cliente page

---
+++ el-buen-sabor-frontend/src/services/ClienteService.ts
@@ -0,0 +1,23 @@
+// src/services/ClienteService.ts
+import axios from "axios";
+import type {IClienteDTO} from "../models/DTO/IClienteDTO";
+
+const API_URL = "http://localhost:8080/api/clientes";
+
+export class ClienteService {
+    async getAllClientes(): Promise<IClienteDTO[]> {
+        const res = await axios.get<IClienteDTO[]>(API_URL);
+        return res.data;
+    }
+    async getClienteById(id: number): Promise<IClienteDTO> {
+        const res = await axios.get<IClienteDTO>(`${API_URL}/${id}`);
+        return res.data;
+    }
+    async updateCliente(id: number, data: Partial<IClienteDTO>): Promise<IClienteDTO> {
+        const res = await axios.put<IClienteDTO>(`${API_URL}/${id}`, data);
+        return res.data;
+    }
+    async cambiarEstado(id: number, activo: boolean): Promise<void> {
+        await axios.patch(`${API_URL}/${id}/estado`, { activo });
+    }
+}
